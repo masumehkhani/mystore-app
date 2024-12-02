a24217f200c803d5efc9281971403abff5c5b58a
masumeh <khanimasume88@gmail.com>
2024-12-02 09:51:37 +0330

    Add colors to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lightbluebg': 'rgba(225, 247, 245, 1)',  
+        'mainbluebg': 'rgba(59, 130, 246, 1)',    
+        'mainbluetext': 'rgba(59, 130, 246, 1)', 
+        'verydarktext':'rgba(51, 65, 85,1)' ,
+        'backgray':'rgba(236,238,239,1)',
+        'nightbg':'rgba(14,35,56,1)'
+      }
+    },
   },
   plugins: [],
 }
